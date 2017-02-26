e42aa74076f3d1759f1816aba95bc7ecee954443
playpool513 <playpool513@gmail.com>
2017-02-26 18:16:46 +0800

    rails_new_revert.sh

---
+++ rails_new_revert.sh
@@ -0,0 +1,21 @@
+echo "Please enter project name:"
+read project_name
+echo
+
+(
+  echo "Rails infrastruction for project '$project_name':\n" &&
+
+  echo "$ cd ~/Projects && pwd" &&
+  cd ~/Projects &&
+  pwd &&
+  echo &&
+
+  echo "$ ll $project_name && rm -rf $project_name" &&
+  ls -l $project_name &&
+  rm -rf $project_name &&
+  echo &&
+
+  echo "$ ll $project_name" &&
+  ls -l $project_name &&
+  echo
+) | tee ~/Projects/\@shell_scripts/rails_infrastruction/project_"$project_name"_revert.rb
