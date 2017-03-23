cf6fec7c8998ec08529b35d0834c2dd67bb2b948
playpool513 <playpool513@gmail.com>
2017-03-23 20:00:55 +0800

    Refactor: remove '&&'

--- rails_new_revert.sh
+++ rails_new_revert.sh
@@ -4,14 +4,14 @@ echo
 set -x
 
 (
-  cd ~/Projects/$project_name &&
-  pwd &&
-  echo &&
+  cd ~/Projects/$project_name
+  pwd
+  echo
 
-  rake db:drop &&
-  echo &&
+  rake db:drop
+  echo
 
-  rm -rf ~/Projects/$project_name &&
-  ls -l ~/Projects/$project_name &&
+  rm -rf ~/Projects/$project_name
+  ls -l ~/Projects/$project_name
   echo
 ) 2>&1 | tee ~/Projects/\@shell_scripts/rails_infrastruction/project_"$project_name"_revert.rb
