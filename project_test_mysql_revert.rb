8e2f9e5231224c44d1730d5290c3adaf8c21c565
playpool513 <playpool513@gmail.com>
2017-03-23 20:04:39 +0800

    Output after refactor

---
+++ project_test_mysql_revert.rb
@@ -0,0 +1,15 @@
++ cd /Users/apple/Projects/test_mysql
++ pwd
+/Users/apple/Projects/test_mysql
++ echo
+
++ rake db:drop
+Database 'test_mysql_development' does not exist
+Database 'test_mysql_test' does not exist
++ echo
+
++ rm -rf /Users/apple/Projects/test_mysql
++ ls -l /Users/apple/Projects/test_mysql
+ls: /Users/apple/Projects/test_mysql: No such file or directory
++ echo
+
