06938be08e5f11b9ce6fd8a40baa073500e7ad37
playpool513 <playpool513@gmail.com>
2017-06-05 17:41:29 +1000

    Add comments

--- rails_new.sh
+++ rails_new.sh
@@ -7,8 +7,10 @@ set -x
 output_file="$(pwd)/project_"$project_name".rb"
 
 (
+  # 1st commit
   command="rails new $project_name -T -d mysql"
   msg="$ $command"
+
   cd ~/Projects
   pwd
   $command
@@ -19,15 +21,19 @@ output_file="$(pwd)/project_"$project_name".rb"
   git commit -m "$msg"
   echo
 
+  # 2nd commit
   msg="Add README.md"
+
   (ruby -v; rails -v) 2>&1 | tee ~/Projects/$project_name/README.md
   git status
   git add .
   git commit -m "$msg"
   echo
 
+  # 3rd commit
   msg="Fix error message: Expected string default value for '--rc'; got false (boolean)"
   append_text="gem 'thor', '0.19.1'"
+
   echo $append_text >> Gemfile
   bundle update thor
   git status
@@ -35,19 +41,22 @@ output_file="$(pwd)/project_"$project_name".rb"
   git commit -m "$msg"
   echo
 
+  # 4th commit
   msg="Fix README.md"
+
   (ruby -v; rails -v) 2>&1 | tee ~/Projects/$project_name/README.md
   git status
   git add .
   git commit -m "$msg"
   echo
 
+  # 5th commit
   database_file="config/database.yml"
   secrets_file="config/secrets.yml"
   database_eg_file="config/database.yml.example"
   secrets_eg_file="config/secrets.yml.example"
-
   msg="Add $database_eg_file & $secrets_eg_file"
+
   cp $database_file $database_eg_file
   cp $secrets_file $secrets_eg_file
   sed -i '' "s/password:$/password: \"fill_in_mysql_password\"/g" $database_eg_file
@@ -57,8 +66,10 @@ output_file="$(pwd)/project_"$project_name".rb"
   git commit -m "$msg"
   echo
 
+  # 6th commit
   msg="Modify $database_file & $secrets_file and add them to .gitignore"
   append_text="\n# config\n/$database_file\n/$secrets_file"
+
   echo $append_text >> .gitignore
   git rm --cached $database_file
   git rm --cached $secrets_file
@@ -70,16 +81,20 @@ output_file="$(pwd)/project_"$project_name".rb"
   git commit -m "$msg"
   echo
 
+  # 7th commit
   command="rake db:create db:migrate"
   msg="$ $command"
+
   $command
   git status
   git add .
   git commit -m "$msg"
   echo
 
+  # 8th commit
   msg="Add gem 'awesome_rails_console'"
   append_text="gem 'awesome_rails_console'"
+
   echo $append_text >> Gemfile
   bundle install
   git status
